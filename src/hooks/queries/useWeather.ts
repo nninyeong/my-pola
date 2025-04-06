188ce2e430822e8e439a5ddec8b325adac67f013
nninyeong <minyk0512@gmail.com>
2025-04-06 21:39:15 +0900

    bugfix: 위치 정보 에러 발생시 기본 위치(서울) 사용하도록 변경

--- src/hooks/queries/useWeather.ts
+++ src/hooks/queries/useWeather.ts
@@ -2,14 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEYS } from './queryKeys';
 import { determineWeatherState, fetchWeather } from '@/services/weather/getWeather';
 import { Position } from '@/types/position.types';
+import { DEFAULT_WEATHER_POSITION } from '@/constants/weatherApi.config';
 
 export const useWeather = (position: Position | null) => {
   return useQuery({
     queryKey: QUERY_KEYS.weather(),
-    queryFn: () => fetchWeather(position),
+    queryFn: () => fetchWeather(position || DEFAULT_WEATHER_POSITION),
     select: determineWeatherState,
     retry: 1,
     staleTime: 1000 * 60 * 10,
-    enabled: !!position,
   });
 };
