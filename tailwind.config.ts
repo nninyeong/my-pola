32d98c89ea148c8a30d0258f8d657168e78b66d5
Subin, Han <ella0411@naver.com>
2025-04-07 15:04:51 +0900

    style: 소셜 로그인 디자인 반영

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        desktop: '1440px',
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
