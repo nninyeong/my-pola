706169310131c80442049184ece99c6e879ec318
Subin, Han <ella0411@naver.com>
2025-04-07 13:41:24 +0900

    feat: 카카오 계정 존재 시 로그인 우회 대신 알림 처리로 변경

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,7 +1,16 @@
 'use client';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { handleAuthCallback } from '@/utils/auth/authCallbackhandler';
 import { handleGoogleSignIn, handleKakaoSignIn, handleNaverSignIn } from '@/utils/supabase/singIn';
 
 const SigInPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    handleAuthCallback(router);
+  }, []);
+
   return (
     <div className='flex flex-col gap-10'>
       <button onClick={handleGoogleSignIn}>구글 로그인</button>
