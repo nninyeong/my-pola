592f34b514f3487b076d5934f88da0ca2eb21c89
nninyeong <minyk0512@gmail.com>
2025-04-09 01:48:11 +0900

    feat: 유저 닉네임, 마이폴라 문구 렌더링

--- src/app/mypola/page.tsx
+++ src/app/mypola/page.tsx
@@ -1,8 +1,16 @@
 import MypolaContainer from '@/components/mypola/MypolaContainer';
 import FixedEllipseBackground from '@/components/background/FixedEllipseBackground';
-export default function page() {
+import { getCurretnUserNickname } from '@/utils/supabase/user';
+
+export default async function page() {
+  const currentUserNickname = await getCurretnUserNickname();
+
   return (
-    <div>
+    <div className='relative'>
+      <h1 className='flex flex-col justify-center items-start text-neutral-1000 font-semibold text-[20px] select-none'>
+        <span>{currentUserNickname}님의 폴라가</span>
+        <span>성장하고 있어요!</span>
+      </h1>
       <FixedEllipseBackground />
       <MypolaContainer />
     </div>
