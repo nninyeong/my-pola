ad2f580457b090ff80890be495fc1e682c85a7f4
nninyeong <minyk0512@gmail.com>
2025-04-09 01:21:04 +0900

    feat: 마이폴라, 소개페이지 fixed 배경 추가

--- src/app/mypola/page.tsx
+++ src/app/mypola/page.tsx
@@ -1,5 +1,10 @@
 import MypolaContainer from '@/components/mypola/MypolaContainer';
-
+import FixedEllipseBackground from '@/components/background/FixedEllipseBackground';
 export default function page() {
-  return <MypolaContainer />;
+  return (
+    <div>
+      <FixedEllipseBackground />
+      <MypolaContainer />
+    </div>
+  );
 }
