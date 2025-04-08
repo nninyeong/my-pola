440da9bfd6f863fd3d97382db04351d992852bde
nninyeong <minyk0512@gmail.com>
2025-04-09 02:55:32 +0900

    style: MypolaContainer 내부 요소 임시 위치 설정

--- src/components/mypola/MypolaContainer.tsx
+++ src/components/mypola/MypolaContainer.tsx
@@ -3,6 +3,7 @@
 import { useWeather } from '@/hooks/queries/useWeather';
 import { useGeolocation } from '@/hooks/user/useGeolocation';
 import MileageBar from './MileageBar';
+import { LEVEL_POLA_NAME } from '@/constants/levelInfo';
 type MypolaContainerProps = {
   level: number;
   mileage: number;
@@ -18,13 +19,16 @@ export default function MypolaContainer({ level, mileage }: MypolaContainerProps
 
   return (
     <div className='flex flex-col justify-around items-center h-[520px]'>
-      <h1>현재 날씨: {weather}</h1>
-      <h1>현재 레벨: {level}</h1>
-      <h1>현재 마일리지: {mileage}</h1>
-      <MileageBar
-        mileage={mileage}
-        level={level}
-      />
+      <div className='h-[400px]'>현재 날씨: {weather}</div>
+      <div>
+        <p className='font-semibold text-neutral-1000 text-[20px] text-center mb-[32px]'>
+          LEVEL. {level} {LEVEL_POLA_NAME[`level${level}` as keyof typeof LEVEL_POLA_NAME]}
+        </p>
+        <MileageBar
+          mileage={mileage}
+          level={level}
+        />
+      </div>
     </div>
   );
 }
