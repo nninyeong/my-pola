611f31f7ea9ce985975d7859cc49368cafa157ad
nninyeong <minyk0512@gmail.com>
2025-04-14 10:13:25 +0900

    feat: 유저의 순위와 관계없이 전체 유저가 11명 이상일 경우 총 11명의 순위가 노출되도록 수정

--- src/services/ranking/processRankingData.ts
+++ src/services/ranking/processRankingData.ts
@@ -4,7 +4,7 @@ export type RankingUserType = UserType & {
   rank: number;
 };
 
-const MAX_RANKING_COUNT = 10;
+const MAX_RANKING_COUNT = 11;
 
 export const processRankingData = (ranking: UserType[], currentUserId: string): RankingUserType[] => {
   const topRanking = ranking.slice(0, MAX_RANKING_COUNT).map((user, index) => ({
@@ -15,10 +15,10 @@ export const processRankingData = (ranking: UserType[], currentUserId: string): 
   const currentUserRank = ranking.findIndex((user) => user.id === currentUserId);
 
   if (currentUserRank !== -1 && currentUserRank >= MAX_RANKING_COUNT) {
-    topRanking.push({
+    topRanking[MAX_RANKING_COUNT - 1] = {
       ...ranking[currentUserRank],
       rank: currentUserRank + 1,
-    });
+    };
   }
 
   return topRanking;
