7f1ce3b0c6c5970bb22524903474f91a548e5c1a
nninyeong <minyk0512@gmail.com>
2025-04-17 02:18:28 +0900

    style: 아이템 상점 링크와 마이폴라 사이 간격 조정

--- src/app/mypola/page.tsx
+++ src/app/mypola/page.tsx
@@ -7,9 +7,9 @@ export default async function page() {
   const { nickname, mileage, point, level } = await getCurrentUserData();
 
   return (
-    <div className='relative flex flex-col h-full'>
+    <div className='relative flex flex-col h-full pt-[14px]'>
       <StoreLink point={point} />
-      <div className='flex-1'>
+      <div className='flex-1 mt-[8px]'>
         <MypolaContainer
           level={level}
           mileage={mileage}
