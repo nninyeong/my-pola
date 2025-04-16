77f61394242bba2a455c4af6a6fe734c3a19ee7f
hansub1n <hansubin@hansubin-ui-MacBookAir.local>
2025-04-17 01:14:11 +0900

    style: 피그마 디자인에 맞게 스타일 수정

--- src/components/challenge/ChallengeDetail.tsx
+++ src/components/challenge/ChallengeDetail.tsx
@@ -6,12 +6,12 @@ type ChallengeDetailProps = {
 
 const ChallengeDetail = ({ title, description, tip }: ChallengeDetailProps) => {
   return (
-    <div className='flex flex-col justify-between items-center font-pretendard text-center h-[357px] mt-[10px]'>
+    <div className='flex flex-col justify-between items-center font-pretendard text-center h-[357px] mt-[5px]'>
       <h1 className='font-semibold text-[20px] mb-[14px] leading-[35px]'>{title}</h1>
       <div className='h-[342px] flex flex-col items-center justify-around'>
-        <h3 className='font-medium text-[12px] mb-[18px] w-[313px] leading-[20px]'>{`"${description}"`}</h3>
+        <h3 className='font-medium text-[12px] mb-[18px] w-[345px] leading-[20px]'>{`"${description}"`}</h3>
         <div className='w-[197px] h-[197px] bg-[#D9D9D9]' />
-        <p className='mt-[10px] font-medium text-[12px] mb-[9px] w-[313px]'>
+        <p className='mt-[10px] font-medium text-[12px] mb-[9px] w-[345px]'>
           <span className='text-primary-400'>💡Tip</span>
           <span className='text-neutral-500'> {tip}</span>
         </p>
