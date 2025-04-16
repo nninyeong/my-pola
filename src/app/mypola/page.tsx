62aa31e69794f88050500bbe022c9ccf09d506c8
nninyeong <minyk0512@gmail.com>
2025-04-17 02:16:10 +0900

    feat: 아이템 상점 이동 컴포넌트 구현, 상점 페이지 생성

--- src/app/mypola/page.tsx
+++ src/app/mypola/page.tsx
@@ -1,12 +1,14 @@
 import MypolaContainer from '@/components/mypola/MypolaContainer';
 import FixedEllipseBackground from '@/components/background/FixedEllipseBackground';
 import { getCurrentUserData } from '@/utils/supabase/auth';
+import StoreLink from '@/components/mypola/StoreLink';
 
 export default async function page() {
   const { nickname, mileage, point, level } = await getCurrentUserData();
 
   return (
     <div className='relative flex flex-col h-full'>
+      <StoreLink point={point} />
       <div className='flex-1'>
         <MypolaContainer
           level={level}
---
+++ src/components/mypola/StoreLink.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+type StoreLinkProps = {
+  point: number;
+};
+
+export default function StoreLink({ point }: StoreLinkProps) {
+  return (
+    <div className='flex justify-end items-center gap-[3px]'>
+      <span className='text-primary-400 text-[16px] font-bold leading-[16px]'>{point}P</span>
+      <Link
+        href='/mypola/itemstore'
+        title='아이템 상점'
+      >
+        <Image
+          src='/assets/icons/store.png'
+          alt='아이템 상점'
+          width={18}
+          height={16}
+        />
+      </Link>
+    </div>
+  );
+}
