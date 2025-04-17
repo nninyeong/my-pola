094398acc22bfc6f870d231a19bc8750ab74054d
nninyeong <minyk0512@gmail.com>
2025-04-17 19:33:25 +0900

    style: friends 페이지 이동 링크 스타일 반영

--- src/components/ui/RankingFriendsPageTab.tsx
+++ src/components/ui/RankingFriendsPageTab.tsx
@@ -12,27 +12,31 @@ export default function RankingFriendsPageTab({ href }: RankingFriendsPageTabPro
   const isCurrentPage = pathname.startsWith(href);
 
   return (
-    <div className='flex w-full'>
-      <Link
-        href='/ranking'
-        className={`text-[18px] font-semibold w-1/2 h-[52px] border-b-[2px] text-center leading-[52px] ${
-          isCurrentPage && href === '/ranking'
-            ? 'text-neutral-1000 border-primary-400'
-            : 'text-neutral-1000/20 border-neutral-200'
-        }`}
-      >
-        랭킹확인
-      </Link>
-      <Link
-        href='/friends'
-        className={`text-[18px] font-semibold w-1/2 h-[52px] border-b-[2px] text-center leading-[52px] ${
-          isCurrentPage && href === '/friends'
-            ? 'text-neutral-1000 border-primary-400'
-            : 'text-neutral-1000/20 border-neutral-200'
-        }`}
-      >
-        추가한 친구
-      </Link>
-    </div>
+    <>
+      {href === '/ranking' && (
+        <Link
+          href='/ranking'
+          className={`text-[18px] font-semibold w-1/2 h-[52px] border-b-[2px] text-center leading-[52px] ${
+            isCurrentPage && href === '/ranking'
+              ? 'text-neutral-1000 border-primary-400'
+              : 'text-neutral-1000/20 border-neutral-200'
+          }`}
+        >
+          랭킹확인
+        </Link>
+      )}
+      {href === '/friends' && (
+        <Link
+          href='/friends'
+          className={`text-[18px] font-semibold w-1/2 h-[52px] border-b-[2px] text-center leading-[52px] ${
+            isCurrentPage && href === '/friends'
+              ? 'text-neutral-1000 border-primary-400'
+              : 'text-neutral-1000/20 border-neutral-200'
+          }`}
+        >
+          추가한 친구
+        </Link>
+      )}
+    </>
   );
 }
