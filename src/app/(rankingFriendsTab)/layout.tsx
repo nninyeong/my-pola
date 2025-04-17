23bf10b41988cec23bbd2554481cdbb34cc03874
nninyeong <minyk0512@gmail.com>
2025-04-17 19:13:22 +0900

    feat: 페이지 전환 탭 레이아웃에 추가

--- src/app/(rankingFriendsTab)/layout.tsx
+++ src/app/(rankingFriendsTab)/layout.tsx
@@ -1,5 +1,5 @@
 import { getCurrentUserData } from '@/utils/supabase/auth';
-
+import RankingFriendsPageTab from '@/components/ui/RankingFriendsPageTab';
 export default async function ChallengeLayout({ children }: { children: React.ReactNode }) {
   const currentUser = await getCurrentUserData();
 
@@ -8,7 +8,10 @@ export default async function ChallengeLayout({ children }: { children: React.Re
       <section className='flex flex-col gap-[21px] fixed w-[calc(100%+40px)] left-[-20px] top-[-20px] h-[151px] rounded-[61px] bg-primary-100 px-[41px] pb-[33px] z-[-9]'>
         <h1 className='pt-[72px] desktop:pt-[110px] text-[18px] font-semibold text-center'>랭킹</h1>
       </section>
-      <main className='mt-[58px] flex flex-col flex-1 gap-[50px]'>{children}</main>
+      <div className='flex flex-col h-full mt-[58px]'>
+        <RankingFriendsPageTab href='/ranking' />
+        <main className='flex flex-col flex-1 gap-[50px]'>{children}</main>
+      </div>
     </div>
   );
 }
---
+++ src/components/ui/RankingFriendsPageTab.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+type RankingFriendsPageTabProps = {
+  href: '/ranking' | '/friends';
+};
+
+export default function RankingFriendsPageTab({ href }: RankingFriendsPageTabProps) {
+  const pathname = usePathname();
+  const isCurrentPage = pathname.startsWith(href);
+
+  return (
+    <div className='flex w-full'>
+      <Link
+        href='/ranking'
+        className={`text-[18px] font-semibold w-1/2 h-[52px] border-b-[2px] text-center leading-[52px] ${
+          isCurrentPage && href === '/ranking'
+            ? 'text-neutral-1000 border-primary-400'
+            : 'text-neutral-1000/20 border-neutral-200'
+        }`}
+      >
+        랭킹확인
+      </Link>
+      <Link
+        href='/friends'
+        className={`text-[18px] font-semibold w-1/2 h-[52px] border-b-[2px] text-center leading-[52px] ${
+          isCurrentPage && href === '/friends'
+            ? 'text-neutral-1000 border-primary-400'
+            : 'text-neutral-1000/20 border-neutral-200'
+        }`}
+      >
+        추가한 친구
+      </Link>
+    </div>
+  );
+}
