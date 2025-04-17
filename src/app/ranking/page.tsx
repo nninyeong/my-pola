16ad007e910a1fd552c929f09ece44fa25f2769b
nninyeong <minyk0512@gmail.com>
2025-04-17 10:15:32 +0900

    feat: 친구추가 버튼 UI 생성

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,8 +1,14 @@
 import Ranking from '@/components/ranking/Ranking';
+import FriendUrlCopyButton from '@/components/ui/buttons/FriendUrlCopyButton';
 import { getCurrentUserData } from '@/utils/supabase/auth';
 
 export default async function page() {
   const currentUser = await getCurrentUserData();
 
-  return <Ranking currentUser={currentUser} />;
+  return (
+    <>
+      <Ranking currentUser={currentUser} />
+      <FriendUrlCopyButton currentUserId={currentUser?.id} />
+    </>
+  );
 }
---
+++ src/components/ui/buttons/FriendUrlCopyButton.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Image from 'next/image';
+
+type FriendUrlCopyButtonProps = {
+  currentUserId: string;
+};
+
+export default function FriendUrlCopyButton({ currentUserId }: FriendUrlCopyButtonProps) {
+  const handleCopyFriendUrl = () => {};
+
+  return (
+    <button
+      onClick={handleCopyFriendUrl}
+      className='fixed bottom-[85px] left-1/2 -translate-x-1/2 bg-primary-300 w-[339px] h-[68px] flex rounded-[8px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]'
+    >
+      <Image
+        src='/assets/icons/pola-head.webp'
+        alt='마이폴라'
+        width={108.34}
+        height={110}
+        className='object-cover'
+      />
+      <p className='flex flex-col justify-center items-center text-[14px] font-semibold text-neutral-100'>
+        <span>'친구와 함께 탄소절감을 실천해보세요'</span>
+        <span>URL</span>
+      </p>
+    </button>
+  );
+}
