b6050881696677edb395fbe2931c64e6637abc0b
hansub1n <hansubin@hansubin-ui-MacBookAir.local>
2025-04-21 00:42:34 +0900

    feat: 닉네임 수정 페이지 뒤로가기 버튼 추가

--- src/app/mypage/edit-nickname/page.tsx
+++ src/app/mypage/edit-nickname/page.tsx
@@ -3,29 +3,42 @@ import UserProfileField from '@/components/mypage/UserProfileField';
 import Button from '@/components/ui/buttons/Button';
 import { useNicknameValidation } from '@/hooks/queries/useNicknameValidation';
 import useUserStore from '@/stores/useUserStore';
+import Image from 'next/image';
 import { useRef } from 'react';
+import PrevIcon from '/public/assets/icons/PrevIcon.png';
+import { useRouter } from 'next/navigation';
 
 const EditNicknamePage = () => {
   const { nickname } = useUserStore();
   const inputRef = useRef<HTMLInputElement>(null);
   const { error, handleBlur, handleSubmit } = useNicknameValidation(nickname, inputRef);
-
+  const router = useRouter();
   return (
-    <div className='mt-[39px]'>
-      <UserProfileField
-        disabled={false}
-        inputRef={inputRef}
-        error={error}
-        handleBlur={handleBlur}
+    <>
+      <Image
+        src={PrevIcon}
+        width={10}
+        height={19}
+        alt='이전 버튼'
+        onClick={() => router.back()}
+        className='fixed top-[55px]'
       />
-      <Button
-        size='xlarge'
-        variant='primary'
-        onClick={handleSubmit}
-      >
-        저장
-      </Button>
-    </div>
+      <div className='mt-[39px]'>
+        <UserProfileField
+          disabled={false}
+          inputRef={inputRef}
+          error={error}
+          handleBlur={handleBlur}
+        />
+        <Button
+          size='xlarge'
+          variant='primary'
+          onClick={handleSubmit}
+        >
+          저장
+        </Button>
+      </div>
+    </>
   );
 };
 
