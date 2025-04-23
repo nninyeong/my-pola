37374aaf1dfe9ff93f27a957e66c3eeb3a2dc19c
nninyeong <minyk0512@gmail.com>
2025-04-23 22:42:37 +0900

    style: 데스크탑 친구목록 정렬

--- src/app/(rankingFriendsTab)/ranking/page.tsx
+++ src/app/(rankingFriendsTab)/ranking/page.tsx
@@ -15,7 +15,7 @@ export default async function page() {
           <h2 className='text-[40px] font-semibold text-neutral-1000 w-full'>랭킹 확인</h2>
           <p className='text-[20px] font-medium text-primary-400 w-full'>나와 친구들의 랭킹을 확인해보세요!</p>
         </div>
-        <div className='flex flex-row justify-center items-center gap-[48px] w-[1200px]'>
+        <div className='flex flex-row justify-center items-start gap-[48px] w-[1200px]'>
           <Ranking currentUser={currentUser} />
           <FriendsList />
         </div>
