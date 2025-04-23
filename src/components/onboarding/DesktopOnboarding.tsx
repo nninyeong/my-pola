0b5f98f567babea8b8f73b6f582bca21d50d5906
nninyeong <minyk0512@gmail.com>
2025-04-23 11:38:15 +0900

    feat: desktop 온보딩 서비스소개 섹션 추가

---
+++ src/components/onboarding/DesktopOnboarding.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+
+export default function DesktopOnboarding() {
+  return (
+    <div className='w-full h-full hidden desktop:block'>
+      <section className='w-full h-[837px] bg-primary-100'>
+        <div className='flex flex-row justify-evenly items-center max-w-[1920px] mx-auto h-full'>
+          <div>
+            <h2 className='text-primary-500 text-[65px] font-semibold mb-[120px]'>마이폴라</h2>
+            <div className='text-neutral-900 text-[40px] font-semibold'>
+              <p>데일리 챌린지를 통해</p>
+              <p>
+                나만의 <span className='text-primary400'>폴라</span>를 성장시키며
+              </p>
+              <p>
+                <span className='text-primary400'>탄소 절감</span>을 즐겁게 시작해보세요!
+              </p>
+            </div>
+          </div>
+          <div className='relative bg-red-100 w-[317px] h-[547px]'>
+            <div className='absolute w-full h-full z-[1]'>
+              <Image
+                src='/assets/images/mypola/mypola3.webp'
+                alt='마이폴라'
+                fill={true}
+                className='object-contain'
+              />
+            </div>
+            <div className='absolute w-full h-full z-[0] flex justify-center items-end pb-[60px]'>
+              <div className='relative w-[210px] h-[52px]'>
+                <Image
+                  src='/assets/images/mypola/mypola-shadow.webp'
+                  alt=''
+                  fill={true}
+                  className='object-contain'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
