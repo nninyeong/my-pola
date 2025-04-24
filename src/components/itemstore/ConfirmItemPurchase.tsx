3a425490344020e7a6dd2329ed07794392be41ef
hansub1n <hansubin@hansubin-ui-MacBookAir.local>
2025-04-24 17:14:04 +0900

    style: 바텀 시트 레이아웃 및 디자인 요소 조정

--- src/components/itemstore/ConfirmItemPurchase.tsx
+++ src/components/itemstore/ConfirmItemPurchase.tsx
@@ -7,11 +7,13 @@ type ConfirmItemPurchaseProps = {
 const ConfirmItemPurchase = ({ selectedItem }: ConfirmItemPurchaseProps) => {
   return (
     <div className='flex flex-col items-center font-pretendard text-center'>
-      <h1 className=' font-semibold text-[24px] mb-[8px]'>구매하시겠습니까?</h1>
+      <h1 className='font-semibold text-[24px] mb-[8px]'>구매하시겠습니까?</h1>
       <div className='w-[260px] h-[181px] bg-[#D9D9D9]' />
-
-      <h3 className='mt-[10px] font-medium text-[20px] mb-[18px]'>{selectedItem.title}</h3>
-      <p className='font-medium text-[18px] text-[#4A4A4A] mb-[9px]'>{selectedItem.description}</p>
+      <h2 className='font-semibold text-[26px] text-primary-400'>{selectedItem.price}P</h2>
+      <h3 className='mt-[21px] font-semibold text-[20px] text-neutral-900 leading-[27px] mb-[2px]'>
+        {selectedItem.title}
+      </h3>
+      <p className='font-semibold text-[12px] text-neutral-700 leading-[27px]'>{selectedItem.description}</p>
     </div>
   );
 };
