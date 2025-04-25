aa5a8f3d7d0aa7ed308c9f23130a151e398cdb62
hansub1n <hansubin@hansubin-ui-MacBookAir.local>
2025-04-25 21:44:49 +0900

    refactor: Modal → InfoModal로 네이밍 변경

--- src/app/(rankingFriendsTab)/friends/page.tsx
+++ src/app/(rankingFriendsTab)/friends/page.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import Modal from '@/components/ui/modal/InfoModal';
+import InfoModal from '@/components/ui/modal/InfoModal';
 import { ADD_FRIEND_RESPONSE_MAP } from '@/constants/addFriendResponse';
 import { useEffect } from 'react';
-import useModalStore from '@/stores/useInfoModalStore';
+import useInfoModalStore from '@/stores/useInfoModalStore';
 import FriendsList from '@/components/ranking/FriendsList';
 
 export default function Page({ searchParams }: { searchParams: { friendStatus?: string; friendName?: string } }) {
   const { friendStatus, friendName } = searchParams;
   const statusCode = friendStatus ? parseInt(friendStatus) : null;
-  const { open } = useModalStore();
+  const { open } = useInfoModalStore();
 
   useEffect(() => {
     if (statusCode && statusCode in ADD_FRIEND_RESPONSE_MAP) {
@@ -20,12 +20,12 @@ export default function Page({ searchParams }: { searchParams: { friendStatus?: 
   return (
     <div>
       {statusCode && statusCode in ADD_FRIEND_RESPONSE_MAP && (
-        <Modal>
+        <InfoModal>
           <p className='text-[16px] text-neutral-900 font-semibold leading-[16px]'>
             <span>{friendName}</span>
             {ADD_FRIEND_RESPONSE_MAP[statusCode as keyof typeof ADD_FRIEND_RESPONSE_MAP].message}
           </p>
-        </Modal>
+        </InfoModal>
       )}
       <FriendsList />
     </div>
