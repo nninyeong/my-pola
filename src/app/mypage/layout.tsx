b8f7a382d2d86039bb943124b8f34d121a4877ef
hansub1n <hansubin@hansubin-ui-MacBookAir.local>
2025-04-28 20:09:10 +0900

    style: 마이페이지 레이아웃 수정

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,12 +1,10 @@
 export default async function ChallengeLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex flex-col h-full desktop:-mx-[20px]'>
+    <div className='desktop:bg-neutral-200/[40%] desktop:mx-auto min-h-screen'>
       <h1 className='desktop:hidden fixed top-[47px] left-1/2 -translate-x-1/2 font-semibold text-[18px] leading-[35px]'>
         마이페이지
       </h1>
-      <main className='pt-[64px] desktop:pt-[138px] desktop:mx-auto desktop:w-desktop desktop:bg-neutral-200/[40%] desktop:h-full'>
-        {children}
-      </main>
+      <main className='pt-[64px] desktop:pt-[138px] desktop:w-desktop desktop:px-[20px]'>{children}</main>
     </div>
   );
 }
