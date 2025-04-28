7079cd8eb35ab8fa89623a0a135dace3ccf4616a
hansub1n <hansubin@hansubin-ui-MacBookAir.local>
2025-04-28 23:33:39 +0900

    style: 챌린지 페이지 레이아웃 mx-auto 추가
    가운데 정렬

--- src/app/challenge/layout.tsx
+++ src/app/challenge/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default async function ChallengeLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='desktop:flex desktop:justify-center desktop:bg-primary-100/[36%] desktop:min-h-screen'>
+    <div className='desktop:flex desktop:justify-center desktop:bg-primary-100/[36%] desktop:min-h-screen mx-auto'>
       <section className='desktop:hidden flex flex-col gap-[21px] fixed w-[calc(100%+40px)] left-[-20px] top-[-20px] h-[284px] rounded-[61px] bg-primary-100 px-[41px] pb-[33px] z-10'>
         <h1 className='pt-[72px] text-[18px] font-semibold text-center'>챌린지</h1>
       </section>
