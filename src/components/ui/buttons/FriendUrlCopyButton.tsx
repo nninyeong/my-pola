994c6711a93b7d2194bf6cb20f6495c83d6301f7
nninyeong <minyk0512@gmail.com>
2025-04-28 22:01:31 +0900

    style: 친구추가 버튼 정렬 깨지지않도록 수정

--- src/components/ui/buttons/FriendUrlCopyButton.tsx
+++ src/components/ui/buttons/FriendUrlCopyButton.tsx
@@ -18,14 +18,14 @@ export default function FriendUrlCopyButton({ currentUserId }: FriendUrlCopyButt
   return (
     <button
       onClick={handleCopyFriendUrl}
-      className='fixed bottom-[85px] left-1/2 -translate-x-1/2 bg-primary-300 w-[339px] desktop:w-[1200px] h-[68px] desktop:h-[164px] flex desktop:justify-center desktop:items-center rounded-[8px] desktop:rounded-[30px] desktop:px-[90px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]'
+      className='fixed bottom-[85px] left-1/2 -translate-x-1/2 bg-primary-300 w-[339px] desktop:w-[1200px] h-[68px] desktop:h-[164px] flex justify-center items-center rounded-[8px] desktop:rounded-[30px] desktop:px-[90px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]'
     >
       <Image
         src='/assets/icons/pola-head.webp'
         alt='마이폴라'
         width={237.37}
         height={241}
-        className='object-cover w-[108.34px] h-[110px] desktop:w-[237.37px]'
+        className='object-contain w-[108.34px] h-[110px] desktop:w-[237.37px]'
       />
       <p className='flex flex-col justify-center items-center text-[14px] desktop:text-[32px] font-semibold text-neutral-100 desktop:w-[757px]'>
         <span>&apos;친구와 함께 탄소절감을 실천해보세요&apos;</span>
