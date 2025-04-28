e3947bfc6f25c1e8b3aa88d0dc7e85fadabb4b7c
nninyeong <minyk0512@gmail.com>
2025-04-28 22:59:33 +0900

    feat: 모바일 헤더 (메뉴바가 표시되는 곳 높이) 위치 상수로 분리해 모바일 온보딩 페이지 높이 조정

--- src/app/(header-title-layout)/page.tsx
+++ src/app/(header-title-layout)/page.tsx
@@ -1,12 +1,13 @@
 import MobileOnboarding from '@/components/onboarding/MobileOnboarding';
 import DesktopOnboarding from '@/components/onboarding/DesktopOnboarding';
 import { getIsSignedIn } from '@/utils/supabase/auth';
+import { MOBILE_HEADER_HEIGHT } from '@/constants/layoutInfo';
 
 export default async function Home() {
   const isSignedIn = await getIsSignedIn();
 
   return (
-    <div className='w-full h-full'>
+    <div className={`w-full h-[calc(100vh-${MOBILE_HEADER_HEIGHT}px)] desktop:h-full`}>
       <MobileOnboarding isSignedIn={isSignedIn} />
       <DesktopOnboarding />
     </div>
