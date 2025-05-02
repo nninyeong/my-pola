a44e5cd420c080ea4f27ae749594effda4bb9cbe
nninyeong <minyk0512@gmail.com>
2025-05-03 01:28:27 +0900

    style: 마이폴라 페이지 레이아웃 데스크탑 스타일 설정

--- src/app/mypola/layout.tsx
+++ src/app/mypola/layout.tsx
@@ -1,8 +1,8 @@
 export default async function MypolaLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <h1 className='fixed top-[41px] left-0 w-full text-[18px] font-semibold text-center'>마이폴라</h1>
-      <div className='h-[calc(100dvh-85px)] overflow-y-auto'>{children}</div>
+      <h1 className='fixed top-[41px] left-0 w-full text-[18px] font-semibold text-center desktop:hidden'>마이폴라</h1>
+      <div className='h-[calc(100dvh-85px)] desktop:h-full overflow-y-auto'>{children}</div>
     </>
   );
 }
