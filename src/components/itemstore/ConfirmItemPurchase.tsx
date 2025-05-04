de33a766cc2d7e185e9929589024d123ef0038e6
hansub1n <hansubin@hansubin-ui-MacBookAir.local>
2025-05-04 18:51:54 +0900

    feat: 아이템 UI에 아이콘 및 착용샷 이미지 적용, 장착 여부 표시 아이콘 추가

--- src/components/itemstore/ConfirmItemPurchase.tsx
+++ src/components/itemstore/ConfirmItemPurchase.tsx
@@ -1,4 +1,5 @@
 import { Item } from '@/types/Item.types';
+import Image from 'next/image';
 
 type ConfirmItemPurchaseProps = {
   selectedItem: Item;
@@ -8,7 +9,14 @@ const ConfirmItemPurchase = ({ selectedItem }: ConfirmItemPurchaseProps) => {
   return (
     <div className='flex flex-col items-center font-pretendard text-center'>
       <h1 className='font-semibold text-[24px] desktop:text-[26px] mb-[8px] desktop:mt-[15px]'>구매하시겠습니까?</h1>
-      <div className='w-[260px] h-[181px] bg-[#D9D9D9]' />
+      <div className='relative w-[100%] h-[181px]'>
+        <Image
+          src={selectedItem.image_url}
+          alt={`${selectedItem.title} 착용샷`}
+          fill
+          className='object-contain'
+        />
+      </div>
       <h2 className='font-semibold text-[26px] text-primary-400'>{selectedItem.price}P</h2>
       <h3 className='mt-[18px] font-semibold text-[20px] text-neutral-900 leading-[27px] mb-[2px]'>
         {selectedItem.title}
--- src/components/itemstore/StoreItemCard.tsx
+++ src/components/itemstore/StoreItemCard.tsx
@@ -1,12 +1,20 @@
 import { Item } from '@/types/Item.types';
+import Image from 'next/image';
 import React from 'react';
 type ItemProps = Item;
 
-const StoreItemCard = ({ title, description, icon_url, image_url, price }: ItemProps) => {
+const StoreItemCard = ({ title, icon_url, price }: ItemProps) => {
   return (
     <div className='rounded-[23px] p-[1px] bg-gradient-to-b from-[#5996e0] to-[#30527A] min-w-[140px] desktop:min-w-[376px] h-[157px] desktop:h-[416px] shadow-primaryCustom'>
       <div className='bg-white rounded-[23px] h-full w-full pb-[10px] desktop:pb-[55px] pt-[22px] desktop:pt-[59px] flex flex-col items-center justify-around font-semibold text-neutral-900'>
-        <div className='h-[75px] desktop:h-[200px] w-[73px] desktop:w-[200px] rounded-full bg-[#d9d9d9] desktop:mb-[17px]' />
+        <div className='relative h-[87px] desktop:h-[322px] w-[100%] desktop:mb-[17px]'>
+          <Image
+            src={icon_url}
+            alt={`${title} 아이콘`}
+            fill
+            className='object-cover'
+          />
+        </div>
         <h1 className='text-[12px] font-semibold desktop:text-[20px] desktop:leading-[35px]'>{title}</h1>
         <p className='text-[12px] font-medium desktop:text-[16px] desktop:leading-[35px]'>{price}P</p>
       </div>
