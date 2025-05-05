c5ee5000921cc498b08c81f0a136fcf11f35b20c
hansub1n <hansubin@hansubin-ui-MacBookAir.local>
2025-05-05 23:53:41 +0900

    style: 데스크탑 아이템 벗기 이미지 높이 수정

--- src/components/itemstore/UnequipItemCard.tsx
+++ src/components/itemstore/UnequipItemCard.tsx
@@ -11,7 +11,7 @@ const UnequipItemCard = ({ onClick }: UnequipItemCard) => {
       className='cursor-pointer desktop:min-w-[376px] desktop:h-[416px] rounded-[23px] p-[1px] bg-gradient-to-b from-[#5996e0] to-[#30527A] min-w-[140px] h-[157px] shadow-primaryCustom'
     >
       <div className='desktop:pb-[55px] desktop:pt-[59px] bg-white rounded-[23px] h-full w-full pb-[10px] pt-[22px] flex flex-col items-center justify-around font-semibold text-neutral-900 '>
-        <div className='relative h-[87px] w-[100%] desktop:mb-[17px] desktop:h-[200px]'>
+        <div className='relative h-[87px] w-[100%] desktop:mb-[17px] desktop:h-[322px]'>
           <Image
             src={UnequippedMyPolarImage}
             alt='아이템 벗은 마이폴라 이미지'
